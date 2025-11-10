d11896641cb165e6d06ca23c4592866750a0fc3f
Harshwardhan <harshawardhandudhal@gmail.com>
2025-11-10 13:51:33 +0530

    FIXED PROBLEM OF COUNTER IN MINUS

--- index.js
+++ index.js
@@ -4,34 +4,34 @@ const displaytext = document.getElementById('displaycounter');
 
 let counter = 0;
 
-// if (incrementcounter.click){
-//     incrementcounter.addEventListener('click', function(){
+    incrementcounter.addEventListener('click', function(){
+            counter = counter + 1;
+            displaytext.textContent = counter;
+            console.log(counter);
+    });
+
+    //DONT PUT EVENT LISTNERS IN IF ELSE BLOCKS.
+    decrementcounter.addEventListener('click', function(){
+        if(counter > 0){
+            counter = counter - 1;
+            displaytext.textContent = counter;
+            console.log(counter);
+        }
+    });
+
+
+
+
+    //SIMPLE CODE BUT COUNTER GOES IN MINUS AFTER DECREMENT.
+// incrementcounter.addEventListener('click', function(){
 //         counter = counter + 1;
 //         displaytext.textContent = counter;
 //         console.log(counter);
 //     });
-// }
-// else{
-//     decrementcounter.addEventListener('click', function(){
-//         counter = counter - 1;
-//         displaytext.textContent = counter;
-//         console.log(counter);
-//     });
-// }
+    
 
-
-incrementcounter.addEventListener('click', function(){
-        counter = counter + 1;
-        displaytext.textContent = counter;
-        console.log(counter);
-    });
-
-    decrementcounter.addEventListener('click', function(){
-        counter = counter - 1;
-        displaytext.textContent = counter;
-        console.log(counter);
-    });
-
-
-    // counter in minus
-    // write code
+// decrementcounter.addEventListener('click', function(){
+//             counter = counter - 1;
+//             displaytext.textContent = counter;
+//             console.log(counter);
+//      });
